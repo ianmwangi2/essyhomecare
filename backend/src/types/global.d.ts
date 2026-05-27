3032af2147a37ea87ce8321d5321f31d4a065c42
Ian Mwangi <ianmwangimacharia49@gmail.com>
2026-05-27 10:49:36 +0300

    changed the backend

--- backend/src/types/global.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" />
-/// <reference types="express" />
-/// <reference types="cors" />
-
-// Ensures CI picks up these ambient type packages when present in node_modules/@types
